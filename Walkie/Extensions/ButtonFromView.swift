0e089623970b4c701c309bca9b55f5fa90f84de7
artemgarshin <garshinareem@gmail.com>
2022-11-08 01:29:25 +0300

    StackView

---
+++ Walkie/Extensions/ButtonFromView.swift
@@ -0,0 +1,40 @@
+import Foundation
+import UIKit
+
+
+class ButtonFormView: UIView{
+    
+    init(label: UILabel, button: UIButton){
+        super.init(frame: .zero)
+        
+        self.translatesAutoresizingMaskIntoConstraints = false // посмтортеь что за функция
+        label.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        self.addSubview(label)
+        self.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: self.topAnchor),
+            label.leadingAnchor.constraint(equalTo: self.leadingAnchor)])
+        
+        
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            button.heightAnchor.constraint(equalToConstant: 60)
+
+            
+        ])
+        
+        bottomAnchor.constraint(equalTo: button.bottomAnchor).isActive = true
+        
+        }
+        
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Walkie/Extensions/UIkit/StackView + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  StackView + Extension.swift
+//  Walkie
+//
+//  Created by Артем Гаршин on 08.11.2022.
+//
+
+import Foundation
